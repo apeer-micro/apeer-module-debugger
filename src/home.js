8df1ca2408306d0b9ba80ae01b3e37006bab6ddf
Nitish Tripathi <m1ntripa@zeiss.com>
2019-01-30 11:02:07 +0100

    reads list of files from selected folder

--- src/home.js
+++ src/home.js
@@ -1,10 +1,18 @@
 const { ipcRenderer } = require('electron')
+const fs = require('fs')
 
 document.getElementById('openFolder').addEventListener('click', _ => {
     ipcRenderer.send('open-folder')
 })
 
-ipcRenderer.on('selected-folder', (event, path) => {
+ipcRenderer.on('selected-folder', (event, paths) => {
+    const path = paths[0]
     var span = document.getElementById('path')
     span.textContent = path
+    
+    fs.readdir(path, (err, files) => {
+        files.forEach(file => {
+            console.log(file)
+        });
+    })
 })
