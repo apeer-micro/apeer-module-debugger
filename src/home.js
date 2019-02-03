93d2b2dae3a388ddc546ba5bd8e31d621fbbd201
Nitish Tripathi <m1ntripa@zeiss.com>
2019-02-03 11:22:31 +0100

    fixes buffer issue, adds run module section

--- src/home.js
+++ src/home.js
@@ -1,7 +1,8 @@
 const { ipcRenderer } = require('electron')
-const { exec } = require('child_process')
+const { exec, spawn } = require('child_process')
 var path = require('path')
 var fs = require('fs')
+const buildStatus = Object.freeze({ "select": 1, "build": 2, "run": 3 })
 
 var moduleFolderPath = ''
 var moduleName = ''
@@ -19,35 +20,57 @@ ipcRenderer.on('selected-folder', (event, paths) => {
     var files = fs.readdirSync(moduleFolderPath)
 
     if (files.find(x => x == 'DockerFile' || x == 'module_specification.json')) {
+        status = buildStatus.build
         document.getElementById('folderSelectError').style.display = 'none'
         selectModuleSection.style.display = 'none'
         buildModuleSection.style.display = 'block'
         document.getElementById('selectedModulePath').innerText = moduleFolderPath
-        var buildModuleLog = ''
-
-        document.getElementById('btnBuildModule').onclick = function () {
-            this.disabled = true;
-            document.getElementById('buildStatus').innerText = "Building Module..."
-
-            const dockerBuildCommand = 'docker build -t ' + moduleName + ' .'
-            console.log(dockerBuildCommand)
-            // const command = 'docker build -t test_python_module -f /Users/m1ntripa/Documents/Modules/test_python_module_286e4ed5-342e-4757-b8fc-185be66d14b3/Dockerfile .'
-
-            const child = exec(dockerBuildCommand, {
-                async: true,
-                cwd: moduleFolderPath
-            })
-            child.stdout.on('data', data => {
-                buildModuleLog = buildModuleLog + data
-                document.getElementById('buildModuleLog').innerText = buildModuleLog
-            })
-
-            child.on('exit', function () {
-                document.getElementById('buildStatus').innerText = "Module Build Complete"
-            })
-        }
     }
     else {
         document.getElementById('folderSelectError').style.display = 'block'
     }
 })
+
+document.getElementById('btnBuildModule').onclick = function () {
+    var buildModuleLog = 'Building module for the first time might take some time, depending on your internet!\n\n'
+    this.disabled = true;
+    document.getElementById('buildStatus').innerText = "Building Module..."
+
+    const dockerBuildCommand = 'docker build -t ' + moduleName + ' .'
+    console.log(dockerBuildCommand)
+    // const command = 'docker build -t test_python_module -f /Users/m1ntripa/Documents/Modules/test_python_module_286e4ed5-342e-4757-b8fc-185be66d14b3/Dockerfile .'
+
+    const child = exec(dockerBuildCommand, {
+        async: true,
+        cwd: moduleFolderPath,
+        maxBuffer: 2000 * 1024
+    })
+
+    child.stdout.on('data', data => {
+        buildModuleLog = buildModuleLog + data
+        document.getElementById('buildModuleLog').innerText = buildModuleLog
+    })
+
+    child.stderr.on('error', data => {
+        console.log('error' + data)
+    })
+
+    child.on('exit', data => {
+        console.log('exit' + data)
+
+        if (data == null || data != 0) {
+            buildModuleLog = buildModuleLog
+                + '\n\nError: The log stream has exited without any code'
+                + '\nPlease run the following command directly in terminal to build your module'
+                + '\n cd ' + moduleFolderPath
+                + '\n' + dockerBuildCommand
+            document.getElementById('buildStatus').innerText = "Module Build Failed"
+
+        }
+        document.getElementById('buildStatus').innerText = "Module Build Complete"
+
+        status = buildStatus.run
+        // document.getElementById('buildModuleSection').style.display = 'none'
+        document.getElementById('runModuleSection').style.display = 'block'
+    })
+}
