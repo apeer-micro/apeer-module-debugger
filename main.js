e6737c87025adf1a510f19dae1f942d0c1a99a95
Michael Hoffmann <kontakt@mokkapps.de>
2019-02-04 22:22:11 +0100

    * Added ESLint & Prettier & Bootstrap CSS styles * Styled header & added some Bootstrap classes * Adapted code to ESLint rules

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
-const {app, BrowserWindow, ipcMain, dialog} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
 
-let mainWindow
+let mainWindow;
 
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 1200, height: 800})
-  const htmlPath = path.join('src', 'home.html')
-  mainWindow.loadFile(htmlPath)
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
+  const htmlPath = path.join('src', 'home.html');
+  mainWindow.loadFile(htmlPath);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', function () {
+app.on('activate', function() {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-ipcMain.on('open-folder', (event) => {
-  dialog.showOpenDialog({
-    properties: ['openDirectory']
-  }, (path => {
-    event.sender.send('selected-folder', path)
-  }))
-})
+ipcMain.on('open-folder', event => {
+  dialog.showOpenDialog(
+    {
+      properties: ['openDirectory']
+    },
+    path => {
+      event.sender.send('selected-folder', path);
+    }
+  );
+});
